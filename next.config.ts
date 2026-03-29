ec5d67c8bb60067c9c5d49bbd9f6d0b2ea63dbfb
BrandonLozada <brandon_lozada@outlook.com>
2026-03-29 17:52:08 -0600

    feat(config): update next.config.ts with image remote patterns and experimental settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  experimental: {
+    viewTransition: false,
+  },
+  cacheComponents: false,
 };
 
 export default nextConfig;
